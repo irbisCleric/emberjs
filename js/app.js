01d77d124bcd87cb62b70d349d8f01eee677e548
Sergii Kokhan <s.kokhan@superdeal.com.ua>
2014-03-19 18:25:54 +0200

    simple event.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,39 @@
-App = Ember.Application.create();
+/*jshint bitwise: false*/
+/*global Ember:false, _:false, md5:false, alertify:false, _predefined:false*/
 
-App.Router.map(function() {
-  // put your routes here
+
+'use strict';
+
+var App = Ember.Application.create({
+    ready: function () {
+//        console.log('Мы это сделали!');
+    }
+});
+
+App.Book = Ember.Object.extend({
+    title: null,
+    author: null,
+    genre: null
+});
+
+App.booksController = Ember.ArrayController.create({
+    content: [],
+    loadBooks: function () {
+        var self = this;
+        $.getJSON('data/books.json', function (data) {
+            data.forEach(function (item) {
+                self.pushObject(App.Book.create(item));
+            });
+        });
+    }
+});
+
+/*App.Router.map(function () {
+    // put your routes here
 });
 
 App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
-});
+    model: function () {
+        return ['red', 'yellow', 'blue'];
+    }
+});*/
