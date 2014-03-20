0b940246679347f481941c9cb38a8d3e7318839b
irbisCleric <s.kokhan@superdeal.com.ua>
2014-03-20 17:35:19 +0200

    Сохранение состояния TODO в модели.

---
+++ js/controllers/todo_controller.js
@@ -0,0 +1,20 @@
+/*jshint bitwise: false*/
+/*global Ember:false, _:false, md5:false, alertify:false, _predefined:false*/
+
+'use strict';
+
+App.TodoController = Ember.ObjectController.extend({
+    isCompleted: function (key, value) {
+        var model = this.get('model');
+
+        if (value === undefined) {
+            // property being used as a getter
+            return model.get('isCompleted');
+        } else {
+            // property being used as a setter
+            model.set('isCompleted', value);
+            model.save();
+            return value;
+        }
+    }.property('model.isCompleted')
+});
