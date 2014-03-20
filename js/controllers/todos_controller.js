7c6342b24b2165c5be1e688b138178cb7eb55b32
irbisCleric <s.kokhan@superdeal.com.ua>
2014-03-20 16:52:27 +0200

    Создание TODO с контроллера.

---
+++ js/controllers/todos_controller.js
@@ -0,0 +1,26 @@
+/*jshint bitwise: false*/
+/*global Ember:false, _:false, md5:false, alertify:false, _predefined:false*/
+
+'use strict';
+
+App.TodosController = Ember.ArrayController.extend({
+    actions: {
+        createTodo: function () {
+            // Get the todo title set by the "New Todo" text field
+            var title = this.get('newTitle');
+            if (!title.trim()) { return; }
+
+            // Create the new Todo model
+            var todo = this.store.createRecord('todo', {
+                title: title,
+                isCompleted: false
+            });
+
+            // Clear the "New Todo" text field
+            this.set('newTitle', '');
+
+            // Save the new model
+            todo.save();
+        }
+    }
+});
