bde37fa9a3ec5b9f2ef45d4349e6c52b943d629c
irbisCleric <s.kokhan@superdeal.com.ua>
2014-04-01 17:59:01 +0300

    Переход к отображению неактивных заданий.

--- js/router.js
+++ js/router.js
@@ -7,6 +7,7 @@ App.Router.map(function () {
     this.resource('todos', { path: '/' }, function () {
         // additional child routes
         this.route('active');
+        this.route('completed');
     });
 });
 
@@ -26,6 +27,17 @@ App.TodosActiveRoute = Ember.Route.extend({
     model: function () {
         return this.store.filter('todo', function (todo) {
             return !todo.get('isCompleted');
+        });
+    },
+    renderTemplate: function (controller) {
+        this.render('todos/index', {controller: controller});
+    }
+});
+
+App.TodosCompletedRoute = Ember.Route.extend({
+    model: function () {
+        return this.store.filter('todo', function (todo) {
+            return todo.get('isCompleted');
         });
     },
     renderTemplate: function (controller) {
