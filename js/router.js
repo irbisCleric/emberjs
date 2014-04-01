f4cc88bf2d549d29bf8b6715eabd06c4e75acccd
irbisCleric <s.kokhan@superdeal.com.ua>
2014-04-01 16:58:40 +0300

    Добавление дочернего роутинга.

--- js/router.js
+++ js/router.js
@@ -4,11 +4,19 @@
 'use strict';
 
 App.Router.map(function () {
-    this.resource('todos', { path: '/' });
+    this.resource('todos', { path: '/' }, function () {
+        // additional child routes
+    });
 });
 
 App.TodosRoute = Ember.Route.extend({
     model: function () {
         return this.store.find('todo');
+    }
+});
+
+App.TodosIndexRoute = Ember.Route.extend({
+    model: function () {
+        return this.modelFor('todos');
     }
 });
